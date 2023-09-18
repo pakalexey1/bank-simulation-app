7d8ac15ae09abc70758399f90b7a26b625041a93
pakalexey <mye604@gmail.com>
2023-09-18 14:55:35 -0400

    Initialized the project

---
+++ src/test/java/com/bank/BankSimulationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSimulationAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
