06a623280bb0e3984c87b42564965341d40ebb6b
pakalexey <mye604@gmail.com>
2023-09-19 16:59:39 -0400

    Continued working on the app.

--- src/main/java/com/bank/exception/BadRequestException.java
+++ src/main/java/com/bank/exception/BadRequestException.java
@@ -2,6 +2,6 @@ package com.bank.exception;
 
 public class BadRequestException extends RuntimeException {
     public BadRequestException(String message) {
-        super(message);//will throw the same message as the RuntimeException does
+        super(message);
     }
 }
