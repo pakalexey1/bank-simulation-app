075d99b19cb55d97261221b446c1372e76fe5854
pakalexey <mye604@gmail.com>
2023-10-09 14:03:23 -0400

    added values to the table

--- src/main/java/com/bank/controller/AccountController.java
+++ src/main/java/com/bank/controller/AccountController.java
@@ -1,10 +1,17 @@
 package com.bank.controller;
 
+import com.bank.enums.AccountType;
+import com.bank.model.Account;
 import com.bank.service.AccountService;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
 
 @Controller
 public class AccountController {
@@ -20,6 +27,26 @@ public class AccountController {
         model.addAttribute("accountList", accountService.listAllAccount());
 
         return "account/index";
+    }
+
+    @GetMapping("/create-form")
+    public String createForm(Model model){
+
+        //provide an empty account object
+        model.addAttribute("account", Account.builder().build()); //the same as new Account()
+
+        //provide accountType enum info to fill the dropdown option
+        model.addAttribute("accountTypes", AccountType.values());
+
+        return "account/create-account";
+    }
+
+    @PostMapping("/create")
+        public String createAccount(@ModelAttribute("account") Account account){
+        System.out.println(account);
+        accountService.createNewAccount(account.getBalance(),new Date(),account.getAccountType(),account.getUserId());
+
+        return "redirect:/index";
     }
 
 }
